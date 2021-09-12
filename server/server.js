dbfd805bed5e1bd7ce508585c098f01a184aae49
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2021-09-11 23:38:23 -0400

    add express client id and secret and app dot post

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const spotifyWebApi = require('spotify-web-api-node');
 const app = express();
+
+app.post('/login', (req, res) => {
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '1270b5698aed42ed830a1ae2ec10e083',
+        clientSecret: '94d5b07f998d472c8b2a63dbec96d735'
+    })
+})
